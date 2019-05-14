fa43d53a3d31668fd40976a9b1e954d1f4211299
tlnob <thais.nobre7@gmail.com>
2019-05-13 23:40:08 -0300

    remove

--- crud.c
+++ crud.c
@@ -11,31 +11,104 @@ void removeReg(char *filein, TregistroDados *reg, char *campo, char *valor_campo
         exit(0);
     }
 
-    int rrn = buscaCampo(fin, reg, campo, valor_campo, cab);
-    printf("rrn: %d\n", rrn);
-    int byte = rrn+16000; // retorna 80 OK
-
-    int topo;
-    fseek(fin, 1, SEEK_SET); 
-    fread(&topo, sizeof(int), 1, fin);
-    printf("topo %d\n", topo);     
-
-    fseek(fin, byte, SEEK_SET); //irá retornar o rrn do registro buscado pulando as páginas de disco iniciais
-    fwrite("*", sizeof(char), 1, fin); 
-    fwrite(&topo, sizeof(int), 1, fin); 
+// 5 arquivo1.bin 11
+// nroInscricao 13893
+// nota 477.5
+// data "08/07/2005"
+// nomeEscola "JOAO ALVARES DE SIQUEIR"
+// cidade "Sao Carlos"
+// cidade "Coimbra"
+// nomeEscola "JOSE CELESTINO ARANHA"
+// nota 667.5
+// data "26/10/2005"
+// nroInscricao 13595
+// nroInscricao 2125
+    int i = 0;
     
+    char buffer[80];
+    int nro;
+    
+    fseek(fin, 16000, SEEK_SET); //setando apos os 16k primeiros bytes
+
+    while(fread(buffer, 80, 1, fin)) { //TODO ele nao funciona para o primeiro registro
+        binarioParaTexto(buffer, &reg[i]);
+        if(strcmp(campo, "nroInscricao") == 0) {
+            nro = atoi(valor_campo);
+            if(nro == reg[i].nroInscricao) {
+                printf("reg[i].nroInscricao %d\n", reg[i].nroInscricao); 
+                printRegistroDados(&reg[i]);
+                handleRemove(i, fin);
+                printf("i %d\n", i);
+                break;
+            } else {
+                i++;
+                continue;
+            }
+        } else if(strcmp(campo, "data") == 0) {
+            if(strcmp(valor_campo, reg[i].data) == 0) {
+                handleRemove(i, fin);                
+            } else {
+                i++;
+                continue;
+            }
+        }  else if(strcmp(campo, "nota") == 0) {
+            double nota = atof(valor_campo);
+            if(nota == reg[i].nota) {
+                handleRemove(i, fin);
+            } else {
+                i++;
+                continue;
+            }
+        } else if(strcmp(campo, "cidade") == 0) {
+            puts("regiii");
+            if(reg[i].tamanho_cidade != 0 && strcmp(valor_campo, reg[i].cidade) == 0) {
+                printRegistroDados(&reg[i]);
+                handleRemove(i, fin);
+            } else {
+                i++;
+                continue;
+            } 
+        } else if (strcmp(campo, "nomeEscola") == 0) {
+            if(reg[i].tamanho_nomeEscola != 0 && strcmp(valor_campo, reg[i].nomeEscola) == 0) {
+                handleRemove(i, fin);
+            } else {
+                i++;
+                continue;
+            } 
+        } else {
+            puts("Registro inexistente.");
+            exit(0);
+        }
+        
+    }
+
+     //   binarioNaTela1(fin);
+    fclose(fin);
+}
+
+void handleRemove(int rrn, FILE *fin) {
+    int topo;
+    rrn = rrn*80;
+    /*Lendo o topoPilha do cabeçalho*/
+    fseek(fin, 1, SEEK_SET);  // inicialmente -1
+    fread(&topo, sizeof(int), 1, fin); //lê o topo na variável "topo"
+    
+    /*Alterando o registro buscado */
+    fseek(fin, rrn+16000, SEEK_SET); //retorna o rrn do registro buscado pulando as páginas de disco iniciais
+    fwrite("*", sizeof(char), 1, fin); //grava * no campo removido
+    fwrite(&topo, sizeof(int), 1, fin); // grava topoPilha no campo encadeamento
+    
+    /* @ no restante dos campos */
     int size = 5; //  1 char e 4 bytes de int 
     while (size < 80) { // preenche com @ o restante
         fputc('@', fin);
         size++;
     }
 
-    cab->topoPilha = rrn; // topo da pilha assume o valor do rrn
-    
+    /*Atualizando o topoPilha no cabeçalho*/
     fseek(fin, 1, SEEK_SET); //para voltar para o cabeçalho e gravar o novo dado do topo da pilha
-    fwrite(&rrn, sizeof(int), 1, fin);
-
-    fclose(fin);
+    fwrite(&rrn, sizeof(int), 1, fin); // gravando no topoPilha rrn do registro
+    
 }
 
 void insert(char *filein, TregistroDados *dados, TregistroCabecalho *cabecalho) {
